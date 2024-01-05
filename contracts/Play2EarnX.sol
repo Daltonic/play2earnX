15047ac87ea987f12c14bb14afa4b6b7bc4b4fea
Daltonic <darlingtongospel@gmail.com>
2024-01-05 17:10:43 +0100

    developed the smart contract

--- contracts/Play2EarnX.sol
+++ contracts/Play2EarnX.sol
@@ -34,7 +34,7 @@ contract PlayToEarnX is Ownable, ReentrancyGuard, ERC20 {
   struct PlayerStruct {
     uint id;
     uint gameId;
-    address acount;
+    address account;
   }
 
   struct InvitationStruct {
@@ -69,7 +69,7 @@ contract PlayToEarnX is Ownable, ReentrancyGuard, ERC20 {
   mapping(uint256 => mapping(address => bool)) isInvited;
   mapping(uint256 => InvitationStruct[]) invitationsOf;
 
-  constructor(uint256 _pct) ERC20('Play To Earn', 'P2E') {
+  constructor(uint256 _pct) ERC20('Play2Earn', 'P2E') {
     servicePct = _pct;
   }
 
@@ -82,7 +82,7 @@ contract PlayToEarnX is Ownable, ReentrancyGuard, ERC20 {
     uint256 endDate
   ) public payable {
     require(msg.value > 0 ether, 'Stake must be greater than zero');
-    require(participants > 1, 'Partiticpants must be greater than one');
+    require(participants > 1, 'Participants must be greater than one');
     require(bytes(title).length > 0, 'Title cannot be empty');
     require(bytes(description).length > 0, 'Description cannot be empty');
     require(startDate > 0, 'Start date must be greater than zero');
@@ -103,15 +103,36 @@ contract PlayToEarnX is Ownable, ReentrancyGuard, ERC20 {
     game.owner = msg.sender;
     game.timestamp = currentTime();
 
-    ScoreStruct memory score;
-    score.id = scores[game.id].length;
-    score.gameId = game.id;
-    score.player = msg.sender;
-    scores[game.id].push(score);
-
     games[game.id] = game;
     gameExists[game.id] = true;
-    require(playedSaved(game.id), 'Failed to create player');
+
+    require(playerCreated(game.id), 'Failed to create player');
+    require(scoreCreated(game.id), 'Failed to create score');
+  }
+
+  function playerCreated(uint gameId) internal returns (bool) {
+    _totalPlayers.increment();
+
+    PlayerStruct memory player;
+    player.id = _totalPlayers.current();
+    player.gameId = gameId;
+    player.account = msg.sender;
+    players[gameId].push(player);
+
+    isListed[gameId][msg.sender] = true;
+    games[gameId].acceptees++;
+
+    totalBalance += msg.value;
+    return true;
+  }
+
+  function scoreCreated(uint gameId) internal returns (bool) {
+    ScoreStruct memory score;
+    score.id = scores[gameId].length;
+    score.gameId = gameId;
+    score.player = msg.sender;
+    scores[gameId].push(score);
+    return true;
   }
 
   function deleteGame(uint256 gameId) public {
@@ -120,34 +141,19 @@ contract PlayToEarnX is Ownable, ReentrancyGuard, ERC20 {
     require(games[gameId].acceptees == 1, 'Participants already in game');
 
     players[gameId].pop();
+    scores[gameId].pop();
+
     isListed[gameId][msg.sender] = false;
     payTo(msg.sender, games[gameId].stake);
 
-    games[gameId].acceptees--;
+    games[gameId].acceptees = 0;
     games[gameId].deleted = true;
-  }
-
-  function playedSaved(uint256 gameId) internal returns (bool) {
-    _totalPlayers.increment();
-
-    PlayerStruct memory player;
-    player.id = _totalPlayers.current();
-    player.gameId = gameId;
-    player.acount = msg.sender;
-
-    players[gameId].push(player);
-
-    isListed[player.gameId][msg.sender] = true;
-
-    games[player.gameId].acceptees++;
-    totalBalance += msg.value;
-    return true;
   }
 
   function invitePlayer(address receiver, uint256 gameId) public {
     require(gameExists[gameId], 'Game not found');
-    require(games[gameId].acceptees <= games[gameId].participants, 'Out of capacity');
-    require(!isListed[gameId][receiver], 'Player is already in this game');
+    require(games[gameId].acceptees < games[gameId].participants, 'Out of capacity');
+    require(!isListed[gameId][receiver], 'Player already accepted');
 
     _totalInvitations.increment();
     InvitationStruct memory invitation;
@@ -166,19 +172,14 @@ contract PlayToEarnX is Ownable, ReentrancyGuard, ERC20 {
 
   function acceptInvitation(uint256 gameId, uint256 index) public payable {
     require(gameExists[gameId], 'Game not found');
-    require(msg.value >= games[gameId].stake, 'Insuffcient funds');
+    require(msg.value >= games[gameId].stake, 'Insuffcient amount');
     require(invitationsOf[gameId][index].receiver == msg.sender, 'Unauthorized entity');
 
-    require(playedSaved(gameId), 'Failed to create player');
+    require(playerCreated(gameId), 'Failed to create player');
+    require(scoreCreated(gameId), 'Failed to create score');
 
     invitationsOf[gameId][index].responded = true;
     invitationsOf[gameId][index].accepted = true;
-
-    ScoreStruct memory score;
-    score.id = scores[gameId].length;
-    score.gameId = gameId;
-    score.player = msg.sender;
-    scores[gameId].push(score);
   }
 
   function rejectInvitation(uint256 gameId, uint256 index) public {
@@ -188,97 +189,98 @@ contract PlayToEarnX is Ownable, ReentrancyGuard, ERC20 {
     invitationsOf[gameId][index].responded = true;
   }
 
+  function playGame(uint256 gameId, uint256 index, uint256 score) public {
+    require(gameExists[gameId], 'Game not found');
+    require(games[gameId].acceptees > games[gameId].numberOfWinners, 'Not enough players yet');
+    require(scores[gameId][index].player == msg.sender, 'Unauthorized entity');
+    require(!scores[gameId][index].played, 'Score already recorded');
+    require(
+      currentTime() > games[gameId].startDate && currentTime() < games[gameId].endDate,
+      'Game play must be in session'
+    );
+
+    scores[gameId][index].score = score;
+    scores[gameId][index].played = true;
+  }
+
   function payout(uint256 gameId) public nonReentrant {
     require(gameExists[gameId], 'Game does not exist');
-    require(currentTime() > games[gameId].endDate, 'Game still in session'); // disable on testing
+    require(currentTime() > games[gameId].endDate, 'Game still in session');
     require(!games[gameId].paidOut, 'Game already paid out');
 
     GameStruct memory game = games[gameId];
 
     uint256 totalStakes = game.stake.mul(game.acceptees);
     uint256 platformFee = (totalStakes.mul(servicePct)).div(100);
-    uint256 creatorFee = platformFee.sub(platformFee.div(2));
+    uint256 creatorFee = platformFee.div(2);
     uint256 gameRevenue = totalStakes.sub(platformFee).sub(creatorFee);
 
     payTo(owner(), platformFee);
     payTo(game.owner, creatorFee);
 
-    ScoreStruct[] memory Scores = new ScoreStruct[](scores[gameId].length);
-    Scores = sortScores(scores[gameId]);
+    ScoreStruct[] memory Results = new ScoreStruct[](scores[gameId].length);
+    Results = sortResult(scores[gameId]);
 
-    for (uint256 i = 0; i < game.numberOfWinners; i++) {
+    for (uint i = 0; i < game.numberOfWinners; i++) {
       uint256 payoutAmount = gameRevenue.div(game.numberOfWinners);
-      payTo(Scores[i].player, payoutAmount);
-      _mint(Scores[i].player, payoutAmount);
+      payTo(Results[i].player, payoutAmount);
+      _mint(Results[i].player, payoutAmount);
 
-      for (uint256 j = 0; j < scores[gameId].length; j++) {
-        if(Scores[i].player == scores[gameId][j].player) {
+      for (uint j = 0; j < scores[gameId].length; j++) {
+        if (Results[i].player == scores[gameId][j].player) {
           scores[gameId][j].prize = payoutAmount;
         }
       }
     }
+
     games[gameId].paidOut = true;
   }
 
-  function sortScores(ScoreStruct[] memory scoreSheet) public pure returns (ScoreStruct[] memory) {
-    uint256 n = scoreSheet.length;
+  function sortResult(
+    ScoreStruct[] memory resultSheet
+  ) internal pure returns (ScoreStruct[] memory) {
+    uint256 counter = resultSheet.length;
 
-    for (uint256 i = 0; i < n - 1; i++) {
-      for (uint256 j = 0; j < n - i - 1; j++) {
+    for (uint i = 0; i < counter - 1; i++) {
+      for (uint j = 0; j < counter - j - 1; j++) {
         // Check if the players played before comparing their scores
-        if (scoreSheet[j].played && scoreSheet[j + 1].played) {
-          if (scoreSheet[j].score > scoreSheet[j + 1].score) {
+        if (resultSheet[j].played && resultSheet[j + 1].played) {
+          if (resultSheet[j].score > resultSheet[j + 1].score) {
             // Swap the elements
-            ScoreStruct memory temp = scoreSheet[j];
-            scoreSheet[j] = scoreSheet[j + 1];
-            scoreSheet[j + 1] = temp;
+            ScoreStruct memory temp = resultSheet[j];
+            resultSheet[j] = resultSheet[j + 1];
+            resultSheet[j + 1] = temp;
           }
-        } else if (!scoreSheet[j].played && scoreSheet[j + 1].played) {
+        } else if (!resultSheet[j].played && resultSheet[j + 1].played) {
           // Sort players who didn't play below players who played
           // Swap the elements
-          ScoreStruct memory temp = scoreSheet[j];
-          scoreSheet[j] = scoreSheet[j + 1];
-          scoreSheet[j + 1] = temp;
+          ScoreStruct memory temp = resultSheet[j];
+          resultSheet[j] = resultSheet[j + 1];
+          resultSheet[j + 1] = temp;
         }
       }
     }
 
-    return scoreSheet;
-  }
-
-  function saveScore(uint256 gameId, uint256 index, uint256 score) public {
-    require(games[gameId].acceptees > games[gameId].numberOfWinners, 'Not enough players yet');
-    require(scores[gameId][index].gameId == gameId, 'Player not found');
-    require(!scores[gameId][index].played, 'Player already recorded');
-    require(scores[gameId][index].player == msg.sender, 'Unauthorized entity');
-    // require(
-    //   currentTime() >= games[gameId].startDate && currentTime() < games[gameId].endDate,
-    //   'Game play must be in session'
-    // ); // disable on testing
-
-    scores[gameId][index].score = score;
-    scores[gameId][index].played = true;
-    scores[gameId][index].player = msg.sender;
+    return resultSheet;
   }
 
   function setFeePercent(uint256 _pct) public onlyOwner {
-    require(_pct > 0 && _pct <= 100, 'Fee percent must be in the range of (1 - 100)');
+    require(_pct > 0 && _pct <= 10, 'Fee percent must be in the range of (1 - 10)');
     servicePct = _pct;
   }
 
   function getGames() public view returns (GameStruct[] memory Games) {
     uint256 available;
-
-    for (uint256 i = 1; i <= _totalGames.current(); i++) {
+    for (uint i = 1; i <= _totalGames.current(); i++) {
       if (!games[i].deleted && !games[i].paidOut) {
         available++;
       }
     }
 
     Games = new GameStruct[](available);
-    uint256 index;
 
-    for (uint256 i = 1; i <= _totalGames.current(); i++) {
+    uint256 index;
+    for (uint i = 1; i <= _totalGames.current(); i++) {
       if (!games[i].deleted && !games[i].paidOut) {
         Games[index++] = games[i];
       }
@@ -287,17 +289,16 @@ contract PlayToEarnX is Ownable, ReentrancyGuard, ERC20 {
 
   function getPaidOutGames() public view returns (GameStruct[] memory Games) {
     uint256 available;
-
-    for (uint256 i = 1; i <= _totalGames.current(); i++) {
+    for (uint i = 1; i <= _totalGames.current(); i++) {
       if (!games[i].deleted && games[i].paidOut) {
         available++;
       }
     }
 
     Games = new GameStruct[](available);
-    uint256 index;
 
-    for (uint256 i = 1; i <= _totalGames.current(); i++) {
+    uint256 index;
+    for (uint i = 1; i <= _totalGames.current(); i++) {
       if (!games[i].deleted && games[i].paidOut) {
         Games[index++] = games[i];
       }
@@ -306,17 +307,16 @@ contract PlayToEarnX is Ownable, ReentrancyGuard, ERC20 {
 
   function getMyGames() public view returns (GameStruct[] memory Games) {
     uint256 available;
-
-    for (uint256 i = 1; i <= _totalGames.current(); i++) {
+    for (uint i = 1; i <= _totalGames.current(); i++) {
       if (!games[i].deleted && games[i].owner == msg.sender) {
         available++;
       }
     }
 
     Games = new GameStruct[](available);
-    uint256 index;
 
-    for (uint256 i = 1; i <= _totalGames.current(); i++) {
+    uint256 index;
+    for (uint i = 1; i <= _totalGames.current(); i++) {
       if (!games[i].deleted && games[i].owner == msg.sender) {
         Games[index++] = games[i];
       }
@@ -327,43 +327,41 @@ contract PlayToEarnX is Ownable, ReentrancyGuard, ERC20 {
     return games[gameId];
   }
 
+  function getScores(uint256 gameId) public view returns (ScoreStruct[] memory) {
+    return scores[gameId];
+  }
+
   function getInvitations(uint256 gameId) public view returns (InvitationStruct[] memory) {
     return invitationsOf[gameId];
   }
 
-  function getMyInvitations() public view returns (InvitationStruct[] memory Invitation) {
+  function getMyInvitations() public view returns (InvitationStruct[] memory Invitations) {
     uint256 available;
-
-    for (uint256 i = 1; i <= _totalGames.current(); i++) {
+    for (uint i = 1; i <= _totalGames.current(); i++) {
       if (isInvited[i][msg.sender]) {
         available++;
       }
     }
 
-    Invitation = new InvitationStruct[](available);
-    uint256 index;
+    Invitations = new InvitationStruct[](available);
 
-    for (uint256 i = 1; i <= _totalGames.current(); i++) {
+    uint256 index;
+    for (uint i = 1; i <= _totalGames.current(); i++) {
       if (isInvited[i][msg.sender]) {
-        for (uint256 j = 0; j < invitationsOf[i].length; j++) {
-          Invitation[index] = invitationsOf[i][j];
-          Invitation[index].id = j;
+        for (uint j = 0; j < invitationsOf[i].length; j++) {
+          Invitations[index] = invitationsOf[i][j];
+          Invitations[index++].id = j;
         }
-        index++;
       }
     }
   }
 
-  function getScores(uint256 gameId) public view returns (ScoreStruct[] memory) {
-    return scores[gameId];
+  function currentTime() internal view returns (uint256) {
+    return (block.timestamp * 1000) + 1000;
   }
 
   function payTo(address to, uint256 amount) internal {
     (bool success, ) = payable(to).call{ value: amount }('');
     require(success);
-  }
-
-  function currentTime() internal view returns (uint256) {
-    return (block.timestamp * 1000) + 1000;
   }
 }
